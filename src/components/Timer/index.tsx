4a3fac9164fcfb6aa1b89425605e986a4ec15ab3
Guillaume Duhan <31253241+guillaumeduhan@users.noreply.github.com>
2025-04-23 13:01:14 -0500

    commit

--- src/components/Timer/index.tsx
+++ src/components/Timer/index.tsx
@@ -11,7 +11,7 @@ const Timer = () => {
     <div className={`relative overflow-hidden dark:border-woodsmoke-900 transition-all duration-500 ease-in-out h-full max-h-[calc(100vh-74px)] overflow-y-auto ${open ? "opacity-100 lg:w-[800px] lg:border-l lg:border-t lg:rounded-tl-xl" : "opacity-0 lg:w-0"
       }`}>
       <List {...{ open, setOpen }} />
-      <div className="absolute bottom-0 sticky w-full h-24 z-50 bg-gradient-to-t from-black to-transparent"></div>
+      <div className="absolute bottom-0 sticky w-full h-24 z-50 bg-gradient-to-t dark:from-black dark:to-transparent"></div>
     </div>
   </div>
 };
