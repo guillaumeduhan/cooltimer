4191ec42cdc11bc183ed57e0327d0494807144bb
Guillaume Duhan <31253241+guillaumeduhan@users.noreply.github.com>
2025-08-07 22:57:28 +0200

    commit

--- src/components/Timer/index.tsx
+++ src/components/Timer/index.tsx
@@ -11,7 +11,17 @@ const backgrounds = [
   "STAR WARS.jpg",
 ];
 
-// Pick a random background once when component mounts
+function formatName(filename: string) {
+  return (
+    filename
+      .replace(/\.jpg$/i, "") // remove old extension
+      .toLowerCase()
+      .split(" ")
+      .map(w => w.charAt(0).toUpperCase() + w.slice(1))
+      .join(" ") + ""
+  );
+}
+
 const Timer = () => {
   const [bg] = useState(
     () => backgrounds[Math.floor(Math.random() * backgrounds.length)]
@@ -23,11 +33,15 @@ const Timer = () => {
         <Header />
         <div className="relative mx-8 rounded-xl grid gap-8 overflow-hidden">
           <div
-            className="absolute overflow-hidden z-10 w-full h-56 bg-cover bg-center bg-no-repeat rounded-xl transition duration-300 ease-in"
+            className="absolute flex items-end justify-end overflow-hidden z-10 w-full h-56 bg-cover bg-center bg-no-repeat rounded-xl transition duration-300 ease-in"
             style={{
               backgroundImage: `url('/backgrounds/${bg}')`,
             }}
-          />
+          >
+            <span className="bg-woodsmoke-950/50 text-woodsmoke-300 text-sm px-2 py-1 rounded m-2">
+              {formatName(bg)}
+            </span>
+          </div>
           <div className="z-50 pt-40">
             <Clock />
           </div>
